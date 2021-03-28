ea6f6140fb60285dc74400e953b272c1b6ffcde7
lethithuhuong99 <huong.le170205@vnuk.edu.vn>
2021-03-28 23:06:23 +0700

    update code

--- Recognize.py
+++ Recognize.py
@@ -3,38 +3,34 @@ import os
 import time
 import pandas as pd
 import warnings
-
 warnings.filterwarnings('ignore')
 import numpy as np
 import cv2
 from tensorflow.keras.models import load_model
 
 model = load_model('MyTrainingModel.h5')
-threshold = 0.90
-
+threshold=0.90
 
 def preprocessing(img):
-    img = img.astype("uint8")
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.equalizeHist(img)
-    img = img / 255
+    img=img.astype("uint8")
+    img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img=cv2.equalizeHist(img)
+    img = img/255
     return img
 
-
 def get_className(classNo):
-    if classNo == 0:
-        return "Mask"
-    elif classNo == 1:
-        return "No Mask"
+	if classNo==0:
+		return "Mask"
+	elif classNo==1:
+		return "No Mask"
 
-
-# -------------------------
+#-------------------------
 def recognize_attendence():
     recognizer = cv2.face.LBPHFaceRecognizer_create()  # cv2.createLBPHFaceRecognizer()
-    recognizer.read("TrainingImageLabel" + os.sep + "Trainner.yml")
+    recognizer.read("TrainingImageLabel"+os.sep+"Trainner.yml")
     harcascadePath = "haarcascade_frontalface_default.xml"
     faceCascade = cv2.CascadeClassifier(harcascadePath)
-    df = pd.read_csv("StudentDetails" + os.sep + "StudentDetails.csv")
+    df = pd.read_csv("StudentDetails"+os.sep+"StudentDetails.csv")
     font = cv2.FONT_HERSHEY_SIMPLEX
     col_names = ['Id', 'Name', 'Date', 'Time']
     attendance = pd.DataFrame(columns=col_names)
@@ -52,22 +48,22 @@ def recognize_attendence():
 
     while True > 0:
         ret, im = cam.read()
-        im = cv2.flip(im, 1)
+        im = cv2.flip(im,1)
+        k = cv2.waitKey(125)
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray, 1.2, 5, minSize=(int(minW), int(minH)),
-                                             flags=cv2.CASCADE_SCALE_IMAGE)
-        for (x, y, w, h) in faces:
-            if (isAttendance == False):
-                Id, conf = recognizer.predict(gray[y:y + h, x:x + w])
-                if (100 - conf) > 76:
+        faces = faceCascade.detectMultiScale(gray, 1.2, 5,minSize = (int(minW), int(minH)),flags = cv2.CASCADE_SCALE_IMAGE)
+        for(x, y, w, h) in faces:
+            if(isAttendance == False):
+                Id, conf = recognizer.predict(gray[y:y+h, x:x+w])
+                if (100-conf) > 76:
 
                     # lấy tên và id
                     cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
                     aa = df.loc[df['Id'] == Id]['Name'].values
                     confstr = "  {0}%".format(round(100 - conf))
-                    tt = str(Id) + "-" + aa
+                    tt = str(Id)+"-"+aa
 
-                    # xử lý điểm danh, lưu vào file
+                    #xử lý điểm danh, lưu vào file
 
                     ts = time.time()
                     date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
@@ -80,7 +76,7 @@ def recognize_attendence():
                     cv2.putText(im, str(tt), (x + 5, y - 5), font, 1, (0, 255, 0), 2)
 
                     # hiển thị tên người điểm danh
-                    cv2.putText(im, str(confstr), (x + 5, y + h - 5), font, 1, (0, 255, 0), 1)
+                    cv2.putText(im, str(confstr), (x + 5, y + h - 5), font,1, (0, 255, 0),1 )
                     print("Da diem danh")
                     isAttendance = True
 
@@ -131,7 +127,7 @@ def recognize_attendence():
                                 cv2.putText(im, str(get_className(classIndex)), (x, y - 10), font, 0.75,
                                             (255, 255, 255), 1,
                                             cv2.LINE_AA)
-                        if (classIndex == 0):
+                        if(classIndex == 0):
                             timeMask = 0
                             mask = str(get_className(classIndex))
                             attendance["Mask"] = mask
@@ -139,9 +135,11 @@ def recognize_attendence():
                             mask = str(get_className(classIndex))
                             attendance["Mask"] = mask
 
+
                     font = cv2.FONT_HERSHEY_SIMPLEX
-                    cv2.putText(im, str(timeMask), (200, 250), font, 7, (0, 255, 255), 4, cv2.LINE_AA)
+                    cv2.putText(im, str(timeMask),(200, 250), font, 7, (0, 255, 255),4, cv2.LINE_AA)
                     cv2.imshow('Attendance', im)
+                    cv2.waitKey(125)
 
                     cur = time.time()
                     if cur - prev >= 1:
@@ -151,14 +149,16 @@ def recognize_attendence():
         attendance = attendance.drop_duplicates(subset=['Id'], keep='first')
         cv2.imshow('Attendance', im)
 
-        if (cv2.waitKey(1) == ord('q')):
+        if (cv2.waitKey(1) == ord('q')) :
             break
     ts = time.time()
     date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
     timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
     Hour, Minute, Second = timeStamp.split(":")
-    fileName = "Attendance" + os.sep + "Attendance_" + date + "_" + Hour + "-" + Minute + "-" + Second + ".csv"
+    fileName = "Attendance"+os.sep+"Attendance_"+date+"_"+Hour+"-"+Minute+"-"+Second+".csv"
     attendance.to_csv(fileName, index=False)
     print("Attendance Successful")
     cam.release()
     cv2.destroyAllWindows()
+
+
